350e3e226b4d0edb59286656917386aa4245bddf
Jaehyeok <six456@naver.com>
2022-11-02 18:49:37 +0900

    초기 설정(에러페이지 처리, AJAX포함,파일)

---
+++ src/main/java/com/gudi/project/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.gudi.project.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	//테스트용 클래스
+	//이거 복붙해서 써요
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		logger.info("정상 실행");
+		model.addAttribute("test", "test" );
+
+		return "test";
+	}
+
+}
---
+++ src/main/java/com/gudi/project/dao/startDAO.java
@@ -0,0 +1,8 @@
+package com.gudi.project.dao;
+
+public interface startDAO {
+/*
+ * 참고용.
+ * start_mapper 확인할 것
+ * */
+}
